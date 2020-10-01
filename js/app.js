58a1b78417d56ab6f33e0068b75961b4a84cc87c
eita-tachino <34756983+eita-tachino@users.noreply.github.com>
2020-10-01 10:58:43 +0900

    history added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,27 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const btns = document.querySelectorAll(".tab-btn");
+const history = document.querySelector(".history");
+const articles = document.querySelectorAll(".content");
+
+// History Tab
+history.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove active from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
